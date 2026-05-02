1a97e0fb8992dc36f32ab5bff6d7b404a18d2a0a
Daniel-De-Dev <id@daniel-de-dev.com>
2026-05-02 12:44:24 +0200

    initial commit
    Set up plain flake that exports a configured treefmt, hopefully i can
    add things that i want available across multiple repos here.

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "Reusable standard tooling";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    inputs@{ flake-parts, treefmt-nix, ... }:
+    let
+      sharedModule =
+        { ... }:
+        {
+          imports = [ treefmt-nix.flakeModule ];
+
+          perSystem =
+            { config, ... }:
+            {
+              treefmt = {
+                projectRootFile = "flake.nix";
+
+                # Nix
+                programs.nixfmt.enable = true;
+                programs.nixfmt.strict = true;
+                programs.nixfmt.width = 80;
+              };
+
+              formatter = config.treefmt.build.wrapper;
+            };
+        };
+    in
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" ];
+
+      imports = [ sharedModule ];
+      flake.flakeModules.default = sharedModule;
+    };
+}
